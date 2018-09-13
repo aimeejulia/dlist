3c211f985157250cf644867e6bbd6cb7e332b1a1
Aimee <aimee@ecohackerfarm.org>
2018-09-13 19:39:11 +0200

    removed close button from nav

--- mainPage.js
+++ mainPage.js
@@ -10,25 +10,17 @@ for (i = 0; i < myNodelist.length; i++) {
   myNodelist[i].appendChild(span);
 }
 
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
+
 
 // Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
   if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
+    ev.target.classList.toggle('completed');
   }
 }, false);
 
-// Create a new list item when clicking on the "Add" button
+// Create a new list item when clicking on the "Add" button or pressing the return key
 function newElement() {
   var li = document.createElement("li");
   var inputValue = document.getElementById("taskInput").value;
