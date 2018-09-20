83226f7d1ea430d4be50471d7e1637f2a15b8c40
Aimee <aimee@ecohackerfarm.org>
2018-09-20 10:30:27 +0200

    started work on local storage management, still far to go

--- mainPage.js
+++ mainPage.js
@@ -1,3 +1,9 @@
+
+// calls the exisiting tasks from the local storage and displays them on the page
+function existingTaskList () {
+  document.getElementById("li").innerHTML = localStorage.getItem("key");
+}
+
 // Create a new list item when clicking on the "Add" button or pressing the return key
 function newElement() {
   var li = document.createElement("li");
@@ -5,20 +11,21 @@ function newElement() {
   var inputValue = document.getElementById("taskInput").value;
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
-  //create a span
+//create a span for delBtn
   var delBtn = document.createElement("span");
   delBtn.className = "delBtn";
   delBtn.innerHTML = "<i class='fas fa-minus'></i>";
   li.appendChild(delBtn);
-
-
+//create a span for checkedBtn
   var checkedBtn = document.createElement("span");
   checkedBtn.className = "checkedBtn";
   checkedBtn.innerHTML = "<i class='fas fa-check'></i>";
   li.appendChild(checkedBtn);
 
+//on clicking checkedBtn, toggleChecked function is called
   checkedBtn.addEventListener("click", toggleChecked);
 
+//on clickingthe add button while the input is empty it will trigger an alert else it will append the content to the list
   if (inputValue === '') {
     alert("You must write something!");
   } else {
@@ -27,13 +34,15 @@ function newElement() {
   document.getElementById("taskInput").value = "";
 };
 
-//Deletes the tasks permanently from the to do list
+//Deletes the tasks permanently from the to do list and  the localStorage
 var task = document.querySelector("#taskUL");
 task.addEventListener("click", removeTask);
 function removeTask(e){
 if(e.target.parentElement.classList.contains('delBtn')){
   if(confirm("Are you sure you want to delete this task?")){
     e.target.parentElement.parentElement.remove();
+    //removing it from localStorage
+    localStorage.removeItem("key");
   }
 }
 }
@@ -41,4 +50,9 @@ if(e.target.parentElement.classList.contains('delBtn')){
 //marks tasks as complete
 function toggleChecked (e) {
   e.target.parentElement.parentElement.classList.toggle('taskCompleted');
+}
+
+//saving tasks to localStorage
+function addToLocalStorage () {
+  localStorage.setItem("key", "value");
 }
