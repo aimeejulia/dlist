3fa8a0fe6210840893b7941bd68b6d2c058c23ea
Aimee <aimee@ecohackerfarm.org>
2018-09-25 22:55:39 +0200

    changes to js to include cookies, not successful yet

--- mainPage.js
+++ mainPage.js
@@ -1,9 +1,29 @@
 
 // calls the exisiting tasks from the local storage and displays them on the page
-function existingTaskList () {
-  document.getElementById("li").innerHTML = localStorage.getItem("key");
-}
+function toDoList () {
+  allCookies = document.cookie;
+  }
 
+function addToList(){
+        var toDoList = $.parseJSON($.cookie("toDoList"))
+
+        if (toDoList.length==0||!toDoList){
+            var toDoList=[];
+            toDoList.push(
+                { 'tasks' : this.getAttribute('tasks')
+                 'taskDescription' : this.getAttribute ('taskDescription')}
+              }
+            );
+        }
+        else{
+            toDoList.push(
+                { 'tasks' : this.getAttribute('tasks'),
+                 'taskDescription' : this.getAttribute ('taskDescription')}
+              }
+            );
+        }
+        $.cookie("toDoList", JSON.stringify(toDoList));
+    }
 // Create a new list item when clicking on the "Add" button or pressing the return key
 function newElement() {
   var li = document.createElement("li");
@@ -12,12 +32,13 @@ function newElement() {
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
 
+
 //create a span for delBtn
   var delBtn = document.createElement("span");
   delBtn.className = "delBtn";
   delBtn.innerHTML = "<i class='fas fa-minus'></i>";
   li.appendChild(delBtn);
-  
+
 //create a span for checkedBtn
   var checkedBtn = document.createElement("span");
   checkedBtn.className = "checkedBtn";
@@ -36,7 +57,7 @@ function newElement() {
   document.getElementById("taskInput").value = "";
 };
 
-//Deletes the tasks permanently from the to do list and  the localStorage
+//Deletes the tasks permanently from the to do list and the localStorage
 var task = document.querySelector("#taskUL");
 task.addEventListener("click", removeTask);
 function removeTask(e){
@@ -44,29 +65,29 @@ if(e.target.parentElement.classList.contains('delBtn')){
   if(confirm("Are you sure you want to delete this task?")){
     e.target.parentElement.parentElement.remove();
     //removing it from localStorage
-    localStorage.removeItem("key");
+    localStorage.removeItem("toDoList");
+    return console.log("the task was deleted");
   }
 }
 }
 
-//marks tasks as complete
-function toggleChecked (e) {
-  e.target.parentElement.parentElement.classList.toggle('taskCompleted');
-}
-
-
-//saving tasks to localStorage
-function addToLocalStorage () {
-  localStorage.setItem("key", "value");
-}
-
-=======
 // Add new todo with the enter key
 var input = document.querySelector("#taskInput");
 input.addEventListener("keyup", function(event) {
     event.preventDefault();
     if (event.keyCode === 13) {
         document.querySelector(".addBtn").click();
+        addToLocalStorage("toDoList");
+        return console.log("the task was added");
     }
 });
 
+//marks tasks as complete
+function toggleChecked (e) {
+  e.target.parentElement.parentElement.classList.toggle('taskCompleted');
+}
+
+//saving tasks to localStorage
+function addToLocalStorage () {
+  localStorage.setItem("toDoList", "task");
+}
